57c7c0b629e63d740f3cfe087810bba368cd8af8
James Parrish <jthomasparrishjr@gmail.com>
2023-08-30 06:29:32 -0500

    home page edit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css'; 
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts",
+}
+function RootLayout({children}) {
+  return (
+    <html Lang='en'>
+        <body>
+            <div className ="main">
+                <div className="gradient"/>
+            </div>
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
